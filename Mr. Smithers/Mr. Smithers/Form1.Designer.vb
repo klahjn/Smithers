e9e1e299df05f807dfab77fd901a1146f5a1b5b3
Allen Sullivan <cerebraljuggalo@gmail.com>
2017-03-06 11:16:35 -0600

    More Frontend Changes
    Adding features, tweaking features.  Setting up front end for backend
    work.   Laying framework.  Redesigning UI

---
+++ Mr. Smithers/Mr. Smithers/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(275, 65)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Process"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(3, 8)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(266, 139)
+        Me.CheckedListBox1.TabIndex = 2
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(277, 98)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(48, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Burn"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(275, 8)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(80, 24)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Open"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(277, 36)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(78, 25)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Destination"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Smithers"
+        Me.NotifyIcon1.Visible = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(277, 130)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(37, 17)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "All"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(368, 153)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Smithers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
---
+++ Mr. Smithers/Mr. Smithers/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim grab As String = FolderBrowserDialog1.ShowDialog()
+        If FolderBrowserDialog1.SelectedPath <> "" Then
+            For Each folder As String In System.IO.Directory.GetDirectories(FolderBrowserDialog1.SelectedPath)
+                If IO.Directory.Exists(folder + "\PS3_GAME\") = True Then CheckedListBox1.Items.Add(folder)
+            Next
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i = 0 To CheckedListBox1.Items.Count - 1
+            CheckedListBox1.SelectedIndex = i
+            If CheckedListBox1.GetItemChecked(i) = True Then MsgBox(CheckedListBox1.SelectedItem.ToString)
+        Next
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim grab As String = FolderBrowserDialog1.ShowDialog
+        If IO.Directory.Exists(FolderBrowserDialog1.SelectedPath.ToString) = True Then My.Settings.Output = FolderBrowserDialog1.SelectedPath.ToString : My.Settings.Save()
+        MsgBox(FolderBrowserDialog1.SelectedPath.ToString)
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then CheckBox1.Checked = True : My.Settings.Burn = "Yes" : Exit Sub
+        If CheckBox1.CheckState = CheckState.Unchecked Then CheckBox1.Checked = False : My.Settings.Burn = "No" : Exit Sub
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If IO.File.Exists("C:\Program Files (x86)\ImgBurn\ImgBurn.exe") = True Then My.Settings.ImgburnLoc = "C:\Program Files (x86)\ImgBurn\ImgBurn.exe" : My.Settings.Save()
+        If IO.File.Exists("C:\Program Files\ImgBurn\ImgBurn.exe") = True Then My.Settings.ImgburnLoc = "C:\Program Files\ImgBurn\ImgBurn.exe" : My.Settings.Save()
+        If My.Settings.Burn = "Yes" Then CheckBox1.Checked = True
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.CheckState = CheckState.Checked Then
+            For i = 0 To CheckedListBox1.Items.Count - 1
+                CheckedListBox1.SetItemChecked(i, True)
+            Next
+            CheckBox2.Text = "None"
+            Exit Sub
+        End If
+        If CheckBox2.CheckState = CheckState.Unchecked Then
+            For i = 0 To CheckedListBox1.Items.Count - 1
+                CheckedListBox1.SetItemChecked(i, False)
+            Next
+            CheckBox2.Text = "All"
+        End If
+    End Sub
+End Class
